6d491232baf332208afd9fc0b59250219f1c3707
Ivan Efremov <st8998@gmail.com>
2014-11-14 11:58:36 +0400

    use local component state

--- app/assets/javascripts/hello_world.cljs
+++ app/assets/javascripts/hello_world.cljs
@@ -1,17 +1,16 @@
 (ns joppa
   (:require [reagent.core :as reagent :refer [atom]]))
 
-(def greeting (atom "Joppa Driller"))
-
 (defn greetings-input [value]
   [:input {:type "text"
            :value @value
            :on-change #(reset! value (-> % .-target .-value))}])
 
 (defn main-component []
-  [:div
-   [:h1 @greeting]
-   [greetings-input greeting]])
+  (let [greeting (atom "Joppa Driller")]
+    (fn []
+      [:div [:h1 @greeting]
+       [greetings-input greeting]])))
 
 (reagent/render-component
   [main-component]
